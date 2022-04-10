cb0188278bfe6d29bd7f2307faff72d176ca9a81
ayrjanson <yrjanson24@up.edu>
2022-04-09 20:53:10 -0700

    Initial Commit

---
+++ SillyCity/SillyCityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SillyCityApp.swift
+//  SillyCity
+//
+//  Created by Anna Yrjanson on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SillyCityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
